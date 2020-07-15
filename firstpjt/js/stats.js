6578b5722143f9e0904e5f66a971bafb2d3b7a33
shubhendu3 <63185422+shubhendu3@users.noreply.github.com>
2020-07-15 15:08:21 +0530

    Update stats.js

--- firstpjt/js/stats.js
+++ firstpjt/js/stats.js
@@ -177,3 +177,59 @@ db.collection("tiffen_service_details/saibhavadeesh@gmail.com/acceptedOrders").g
         }
     });
 });
+
+db.collection("customer_collection/p6zMdVeQBsQwNez17mdbXj08rU72/acceptedOrders").get().then(function(querySnapshot){
+    querySnapshot.forEach(function(doc){
+        totalorders++;
+        ;
+    });
+    totalordersCount.innerHTML += "<h4><b>" + totalorders + "</b></h4>";
+
+
+var data = [totalorders];
+var labels = ['JUN']
+new Chart(totalordersChart, {
+    type: 'bar',
+    data: {
+        labels: labels,
+        datasets: [
+            {
+                backgroundColor: '#FBE405',
+                borderColor: '#FBE405',
+                data: data
+            }
+        ]
+    },
+    options: {
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        hover:{
+            mode: 'index',
+            intersect: true
+        },
+        scales:{
+            yAxes: [{
+                gridLines: {
+                    display: true,
+                    lineWidth: '4px',
+                    color: 'rgba(0, 0, 0, 0.2)',
+                    zeroLineColor: 'transparent'
+                },
+                scaleLabel:{
+                    display: true,
+                    labelString: 'Total Orders',
+                    fontColor: 'black',
+                },
+                ticks: {
+                    beginAtZero: false,
+                    stepSize: 2
+                }
+            }]
+        }
+        
+    }
+});
+});
+
