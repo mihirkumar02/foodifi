fd18ba6081d0d15e0707c486ff2aa02e0dec0a61
shubhendu3 <63185422+shubhendu3@users.noreply.github.com>
2020-07-16 12:23:38 +0530

    Add files via upload

---
+++ firstpjt/js/contactUs.js
@@ -0,0 +1,51 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCHXsGkm61Hmk_yLYw5txSPfee_fGiDMDc",
+    authDomain: "foodifi-493e8.firebaseapp.com",
+    databaseURL: "https://foodifi-493e8.firebaseio.com",
+    projectId: "foodifi-493e8",
+    storageBucket: "foodifi-493e8.appspot.com",
+    messagingSenderId: "328551440601",
+    appId: "1:328551440601:web:9a84de1b23ee84403b5ec8",
+    measurementId: "G-DHPTH3J7N4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  //Reference contactUs collection
+let messagesRef = firebase.database().ref('ContactUs');
+
+//listen to form
+document.getElementById('contactform').addEventListener('submit', submitForm);
+
+function submitForm(e){
+    e.preventDefault();
+
+//     // get Values
+    let name = getInputVal('name');
+    let email = getInputVal('email');
+    let message = getInputVal('message');
+
+    //save message
+
+    saveMessage(name, email, message);
+
+    //clear form
+    document.getElementById('contactform').reset();
+}
+//function to get form values
+
+function getInputVal(id){
+    return document.getElementById(id).value;
+}
+
+//save message to firebase
+function saveMessage(name, email, message){
+    let newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        message: message
+    })
+}
